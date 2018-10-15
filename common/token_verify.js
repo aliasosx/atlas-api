ede1f3096eccba1fc44bf75e553db4dc639dd3fb
alisaosx <aliasosx@hotmail.com>
2018-10-15 13:18:52 +0700

    add cors capable

--- common/token_verify.js
+++ common/token_verify.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 
-var privateKey = fs.readFileSync('/Users/loukpa/projects/web/nodejs/atlas-api/common/keys/private.key', 'utf8');
-var publicKey = fs.readFileSync('/Users/loukpa/projects/web/nodejs/atlas-api/common/keys/public.key', 'utf8');
+var privateKey = fs.readFileSync('E:/PROGRAMMING/nodejs/atlas-api/common/keys/private.key', 'utf8');
+var publicKey = fs.readFileSync('E:/PROGRAMMING/nodejs/atlas-api/common/keys/public.key', 'utf8');
 const secret = "loukpa";
 
 module.exports = {
--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var app = require('./common/app');
 var userRouter = require('./routes/user.router');
 var loginRouter = require('./routes/login.router');
 var menus = require('./routes/menu.router');
-
+var cors = require('cors');
 
 var port = process.env.PORT || 5000;
-
+app.use(cors());
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/login', loginRouter);
 app.use('/api/v1/menus', menus);
