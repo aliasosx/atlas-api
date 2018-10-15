6ecc77badae3a92b8c73979141d58ca316e71140
alisaosx <aliasosx@hotmail.com>
2018-10-15 16:53:31 +0700

    db shema

---
+++ schema/atlas.sql
@@ -0,0 +1,248 @@
+-- MySQL dump 10.16  Distrib 10.2.14-MariaDB, for Win64 (AMD64)
+--
+-- Host: 127.0.0.1    Database: atlas
+-- ------------------------------------------------------
+-- Server version	10.2.14-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_card_no` varchar(16) DEFAULT NULL,
+  `gender` char(1) NOT NULL COMMENT 'M as male, F as Female',
+  `fullname` varchar(255) NOT NULL COMMENT 'format as "Name Surname"',
+  `DateofBirth` date NOT NULL,
+  `current_address` varchar(2000) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `photo` varchar(2000) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT current_timestamp(),
+  `enabled` tinyint(1) DEFAULT 1,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `customers_id_uindex` (`id`),
+  UNIQUE KEY `customers_fullname_uindex` (`fullname`),
+  UNIQUE KEY `customers_mobile_uindex` (`mobile`),
+  UNIQUE KEY `customers_id_card_no_uindex` (`id_card_no`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` (`id`, `id_card_no`, `gender`, `fullname`, `DateofBirth`, `current_address`, `mobile`, `photo`, `created_at`, `updated_at`, `enabled`) VALUES (1,'1234567890','M','Soulisack SAYYALINH','1985-02-10','Dongdok','+8562055588857','http://www.fujifilm.co.th/Products/digital_cameras/x/fujifilm_x20/sample_images/img/index/ff_x20_008.JPG','2018-10-15 14:33:15','2018-10-15 14:33:15',1);
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `food_types`
+--
+
+DROP TABLE IF EXISTS `food_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `food_types` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `food_type` varchar(4) NOT NULL,
+  `food_type_desc` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `food_types_id_uindex` (`id`),
+  UNIQUE KEY `food_types_food_type_uindex` (`food_type`),
+  UNIQUE KEY `food_types_food_type_desc_uindex` (`food_type_desc`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `food_types`
+--
+
+LOCK TABLES `food_types` WRITE;
+/*!40000 ALTER TABLE `food_types` DISABLE KEYS */;
+INSERT INTO `food_types` (`id`, `food_type`, `food_type_desc`) VALUES (1,'FOOD','Food'),(2,'DRNK','DRINK');
+/*!40000 ALTER TABLE `food_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `foods`
+--
+
+DROP TABLE IF EXISTS `foods`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `foods` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `food_name` varchar(255) NOT NULL,
+  `photo` varchar(2000) NOT NULL,
+  `food_type_id` int(11) NOT NULL,
+  `cost` double NOT NULL,
+  `price` double NOT NULL,
+  `currcode` varchar(3) NOT NULL,
+  `enabled` tinyint(1) DEFAULT 1,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `foods_id_uindex` (`id`),
+  UNIQUE KEY `foods_food_name_uindex` (`food_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `foods`
+--
+
+LOCK TABLES `foods` WRITE;
+/*!40000 ALTER TABLE `foods` DISABLE KEYS */;
+INSERT INTO `foods` (`id`, `food_name`, `photo`, `food_type_id`, `cost`, `price`, `currcode`, `enabled`, `created_at`, `updated_at`) VALUES (6,'ຊາໄຂ່ມຸກ','https://randomwire.com/wp-content/uploads/plastic-food.jpeg',2,8000,12000,'418',1,NULL,'2018-10-15 16:45:57'),(7,'ກະເພົາໄກ່','https://randomwire.com/wp-content/uploads/plastic-food.jpeg',1,10000,15000,'418',1,NULL,'2018-10-15 16:46:25'),(9,'ຜັກກາດນາໝູກອບ','https://randomwire.com/wp-content/uploads/plastic-food.jpeg',1,10000,15000,'418',1,NULL,'2018-10-15 16:46:39');
+/*!40000 ALTER TABLE `foods` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menus`
+--
+
+DROP TABLE IF EXISTS `menus`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `menus` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `menu_name` varchar(255) NOT NULL,
+  `menu_link` varchar(255) NOT NULL,
+  `menu_icon` varchar(255) NOT NULL,
+  `enabled` tinyint(1) DEFAULT 1,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `menus_id_uindex` (`id`),
+  UNIQUE KEY `menus_menu_name_uindex` (`menu_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menus`
+--
+
+LOCK TABLES `menus` WRITE;
+/*!40000 ALTER TABLE `menus` DISABLE KEYS */;
+INSERT INTO `menus` (`id`, `menu_name`, `menu_link`, `menu_icon`, `enabled`) VALUES (1,'Home','/','menu',1),(2,'Sales','/sales','menu',1),(3,'Terminals','/terminals','menu',1);
+/*!40000 ALTER TABLE `menus` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_code` varchar(20) NOT NULL,
+  `role_name` varchar(20) NOT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT 1,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `roles_id_uindex` (`id`),
+  UNIQUE KEY `roles_role_code_uindex` (`role_code`),
+  UNIQUE KEY `roles_role_name_uindex` (`role_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` (`id`, `role_code`, `role_name`, `enabled`, `created_at`, `updated_at`) VALUES (1,'ADMIN','ADMINISTRATOR',1,'2018-10-07 01:37:02','2018-10-07 01:37:02'),(2,'GUEST','GUEST',1,'2018-10-07 01:37:13','2018-10-07 01:37:13'),(3,'CHEF','Chef',1,'2018-10-07 01:38:06','2018-10-07 01:38:06'),(5,'STAFF','STAFF',1,'2018-10-07 01:38:44','2018-10-07 01:38:44');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tranx_role_menu`
+--
+
+DROP TABLE IF EXISTS `tranx_role_menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tranx_role_menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_code` varchar(20) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `tranx_role_menu_id_uindex` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tranx_role_menu`
+--
+
+LOCK TABLES `tranx_role_menu` WRITE;
+/*!40000 ALTER TABLE `tranx_role_menu` DISABLE KEYS */;
+INSERT INTO `tranx_role_menu` (`id`, `role_code`, `menu_id`) VALUES (1,'ADMIN',1),(4,'ADMIN',2),(5,'ADMIN',3);
+/*!40000 ALTER TABLE `tranx_role_menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(70) NOT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT 1,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT current_timestamp(),
+  `role_code` varchar(20) NOT NULL,
+  `emp_id` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_id_uindex` (`id`),
+  UNIQUE KEY `users_username_uindex` (`username`),
+  UNIQUE KEY `users_email_uindex` (`email`),
+  UNIQUE KEY `users_password_uindex` (`password`),
+  UNIQUE KEY `users_emp_id_uindex` (`emp_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `enabled`, `created_at`, `updated_at`, `role_code`, `emp_id`) VALUES (64,'admin','sayyalinh@gmail.com','$2b$10$AYlGbhNUtNZk7Q1Ygp.FBu6li3s5N/xpb53Lq04tSDYRgnbCwC5IG',0,'2018-10-07 00:10:38','2018-10-07 00:10:38','ADMIN','00000000000000000001');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-10-15 16:52:27
