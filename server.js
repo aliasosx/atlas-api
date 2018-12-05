8a8b506224fe48012210e555abe8ae2861aaa97a
Soulisack SAYYALINH <soulisacksayyalinh@Soulisacks-iMac.local>
2018-12-05 23:02:52 +0700

    add ssl certificate

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var app = require('./common/app');
 var userRouter = require('./routes/user.router');
 var loginRouter = require('./routes/login.router');
+const crypto = require('crypto');
+var fs = require('fs');
+var https = require('https');
 
 
 var routerAtlas = require('./routes/atlas.router');
@@ -14,6 +17,15 @@ app.use('/api/v1/login', loginRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1', routerAtlas);
 
+/*
 var server = app.listen(port, function () {
+    console.log('Atlas-api Server listening on port ' + port);
+});
+*/
+
+https.createServer({
+    key: fs.readFileSync('ssl/server.key'),
+    cert: fs.readFileSync('ssl/server.cert')
+}, app).listen(port, () => {
     console.log('Atlas-api Server listening on port ' + port);
 });
